4f2de7378966d698a51931fecbd42ab400d0393d
MaulingMonkey <git@maulingmonkey.com>
2017-04-06 01:08:13 -0700

    Add support for cloning, destroying VMs.

---
+++ MaulingMonkey.Manage.VMs/VirtualBox/CloneVm.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2017 MaulingMonkey
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MaulingMonkey.Manage.VMs {
+	partial class VirtualBox {
+		public VmNameId CloneVm(VmNameId original                        ) { return CloneVm(original.Guid, new VmNameId() { Name = "Clone of "+original.Name, Guid = Guid.NewGuid().ToString() }); }
+		public VmNameId CloneVm(string   originalGuid                    ) { return CloneVm(originalGuid,  new VmNameId() { Name = "Clone of "+originalGuid , Guid = Guid.NewGuid().ToString() }); }
+		public VmNameId CloneVm(VmNameId original,     string   newName  ) { return CloneVm(original.Guid, new VmNameId() { Name = newName, Guid = Guid.NewGuid().ToString() }); }
+		public VmNameId CloneVm(string   originalGuid, string   newName  ) { return CloneVm(originalGuid,  new VmNameId() { Name = newName, Guid = Guid.NewGuid().ToString() }); }
+		public VmNameId CloneVm(VmNameId original,     VmNameId newNameId) { return CloneVm(original.Guid, newNameId); }
+		public VmNameId CloneVm(string   originalGuid, VmNameId newNameId) {
+			if (VBoxManagePath == null) throw new MissingToolException("VBoxManage", VBoxManage_Paths);
+
+			// TODO:  More streamy stdout parser so we can sanely parse:
+			//      0%...10%...20%...30%...40%...50%...60%...70%...80%...90%...100%
+			//      Machine has been successfully cloned as "Name"
+			var exit = Proc.ExecIn(null, VBoxManagePath, $"clonevm {originalGuid} --register --uuid {newNameId.Guid} --name \"{newNameId.Name}\"", stdout => { }, stderr => { }, ProcessWindowStyle.Hidden);
+			if (exit != 0) throw new ToolResultSyntaxException("VBoxManage clonevm ...", "Returned nonzero");
+			return newNameId;
+		}
+
+		public VmNameId TryCloneVm(VmNameId original                        ) { try { return CloneVm(original               ); } catch (VmManagementException) { return default(VmNameId); } }
+		public VmNameId TryCloneVm(string   originalGuid                    ) { try { return CloneVm(originalGuid           ); } catch (VmManagementException) { return default(VmNameId); } }
+		public VmNameId TryCloneVm(VmNameId original,     string   newName  ) { try { return CloneVm(original    , newName  ); } catch (VmManagementException) { return default(VmNameId); } }
+		public VmNameId TryCloneVm(string   originalGuid, string   newName  ) { try { return CloneVm(originalGuid, newName  ); } catch (VmManagementException) { return default(VmNameId); } }
+		public VmNameId TryCloneVm(VmNameId original,     VmNameId newNameId) { try { return CloneVm(original    , newNameId); } catch (VmManagementException) { return default(VmNameId); } }
+		public VmNameId TryCloneVm(string   originalGuid, VmNameId newNameId) { try { return CloneVm(originalGuid, newNameId); } catch (VmManagementException) { return default(VmNameId); } }
+
+		public Task<VmNameId> CloneVmAsync(VmNameId original                        ) { return Task.Run(()=>CloneVm(original               )); }
+		public Task<VmNameId> CloneVmAsync(string   originalGuid                    ) { return Task.Run(()=>CloneVm(originalGuid           )); }
+		public Task<VmNameId> CloneVmAsync(VmNameId original,     string   newName  ) { return Task.Run(()=>CloneVm(original,     newName  )); }
+		public Task<VmNameId> CloneVmAsync(string   originalGuid, string   newName  ) { return Task.Run(()=>CloneVm(originalGuid, newName  )); }
+		public Task<VmNameId> CloneVmAsync(VmNameId original,     VmNameId newNameId) { return Task.Run(()=>CloneVm(original,     newNameId)); }
+		public Task<VmNameId> CloneVmAsync(string   originalGuid, VmNameId newNameId) { return Task.Run(()=>CloneVm(originalGuid, newNameId)); }
+
+		public Task<VmNameId> TryCloneVmAsync(VmNameId original                        ) { return Task.Run(()=>TryCloneVm(original               )); }
+		public Task<VmNameId> TryCloneVmAsync(string   originalGuid                    ) { return Task.Run(()=>TryCloneVm(originalGuid           )); }
+		public Task<VmNameId> TryCloneVmAsync(VmNameId original,     string   newName  ) { return Task.Run(()=>TryCloneVm(original,     newName  )); }
+		public Task<VmNameId> TryCloneVmAsync(string   originalGuid, string   newName  ) { return Task.Run(()=>TryCloneVm(originalGuid, newName  )); }
+		public Task<VmNameId> TryCloneVmAsync(VmNameId original,     VmNameId newNameId) { return Task.Run(()=>TryCloneVm(original,     newNameId)); }
+		public Task<VmNameId> TryCloneVmAsync(string   originalGuid, VmNameId newNameId) { return Task.Run(()=>TryCloneVm(originalGuid, newNameId)); }
+	}
+}
---
+++ MaulingMonkey.Manage.VMs/VirtualBox/DeleteVm.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2017 MaulingMonkey
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MaulingMonkey.Manage.VMs {
+	partial class VirtualBox {
+		public void DeleteVm(VmNameId target) { DeleteVm(target.Guid); }
+		public void DeleteVm(string   target) {
+			if (VBoxManagePath == null) throw new MissingToolException("VBoxManage", VBoxManage_Paths);
+
+			// TODO:  More streamy stdout parser so we can sanely parse:
+			//      0%...10%...20%...30%...40%...50%...60%...70%...80%...90%...100%
+			var exit = Proc.ExecIn(null, VBoxManagePath, $"unregistervm --delete {target}", stdout => { }, stderr => { }, ProcessWindowStyle.Hidden);
+			if (exit != 0) throw new ToolResultSyntaxException("VBoxManage unregistervm --delete ...", "Returned nonzero");
+		}
+
+		public bool       TryDeleteVm     (VmNameId target) { try { DeleteVm(target); return true; } catch (VmManagementException) { return false; } }
+		public bool       TryDeleteVm     (string   target) { try { DeleteVm(target); return true; } catch (VmManagementException) { return false; } }
+
+		public Task       DeleteVmAsync   (VmNameId target) { return Task.Run(()=>DeleteVm(target)); }
+		public Task       DeleteVmAsync   (string   target) { return Task.Run(()=>DeleteVm(target)); }
+
+		public Task<bool> TryDeleteVmAsync(VmNameId target) { return Task.Run(()=>TryDeleteVm(target)); }
+		public Task<bool> TryDeleteVmAsync(string   target) { return Task.Run(()=>TryDeleteVm(target)); }
+	}
+}
